128f8ad0ddca1c69e4542e41f5dfb9a261a0ce57
votranquy <votranquy96@gmail.com>
2018-11-13 22:45:19 +0700

    AddUser

--- resources/views/admin/user/them.blade.php
+++ resources/views/admin/user/them.blade.php
@@ -1,58 +1,62 @@
 @extends('admin.layout.index')
-
 @section('content')
- <!-- Page Content -->
-        <div id="page-wrapper">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">Category
-                            <small>Add</small>
-                        </h1>
+                  <!-- general form elements -->
+                  <div class="box box-primary">
+                    <div class="box-header with-border">
+                      <h3 class="box-title">Thêm user</h3>
                     </div>
-                    <!-- /.col-lg-12 -->
-                    <div class="col-lg-7" style="padding-bottom:120px">
-                        <form action="" method="POST">
-                            <div class="form-group">
-                                <label>Category Parent</label>
-                                <select class="form-control">
-                                    <option value="0">Please Choose Category</option>
-                                    <option value="">Tin Tức</option>
-                                </select>
+                    <!-- /.box-header -->
+                    <!-- form start -->
+                        @if(count($errors)>0)
+                            <div class="alert alert-danger">
+                                @foreach($errors->all() as $err)
+                                    {{$err}}<br>
+                                @endforeach
                             </div>
-                            <div class="form-group">
-                                <label>Category Name</label>
-                                <input class="form-control" name="txtCateName" placeholder="Please Enter Category Name" />
+                        @endif
+
+                        @if(session('thongbao'))
+                            <div class="alert alert-success">
+                                {{session('thongbao')}}
                             </div>
-                            <div class="form-group">
-                                <label>Category Order</label>
-                                <input class="form-control" name="txtOrder" placeholder="Please Enter Category Order" />
-                            </div>
-                            <div class="form-group">
-                                <label>Category Keywords</label>
-                                <input class="form-control" name="txtOrder" placeholder="Please Enter Category Keywords" />
-                            </div>
-                            <div class="form-group">
-                                <label>Category Description</label>
-                                <textarea class="form-control" rows="3"></textarea>
-                            </div>
-                            <div class="form-group">
-                                <label>Category Status</label>
-                                <label class="radio-inline">
-                                    <input name="rdoStatus" value="1" checked="" type="radio">Visible
-                                </label>
-                                <label class="radio-inline">
-                                    <input name="rdoStatus" value="2" type="radio">Invisible
-                                </label>
-                            </div>
-                            <button type="submit" class="btn btn-default">Category Add</button>
-                            <button type="reset" class="btn btn-default">Reset</button>
-                        <form>
-                    </div>
-                </div>
-                <!-- /.row -->
-            </div>
-            <!-- /.container-fluid -->
-        </div>
-        <!-- /#page-wrapper -->
+                        @endif
+                    <form action="admin/user/them" method="POST">
+                    <input type="hidden" name="_token" value={{csrf_token()}} />
+                      <div class="box-body">
+                        <div class="form-group">
+                          <label>Username</label>
+                          <input class="form-control" id="username" name="username" placeholder="Nhập username">
+                        </div>
+                        <div class="form-group">
+                          <label>PassWord</label>
+                          <input class="form-control" id="password" name="password"placeholder="Nhập password">
+                        </div>
+                        <div class="form-group">
+                          <label>Tên</label>
+                          <input class="form-control" id="name" name="name"placeholder="Nhập Tên bạn">
+                        </div>
+                        <div class="form-group">
+                          <label>Địa chỉ</label>
+                          <input class="form-control" id="address" name="address"placeholder="Nhập địa chỉ">
+                        </div>  
+                        <div class="form-group">
+                          <label>SDT</label>
+                          <input class="form-control" id="phone" name="phone"placeholder="Nhập số điện thoại">
+                        </div>
+                        <div class="form-group">
+                          <label>Ảnh</label>
+                          <input class="form-control" type="file" id="image" name="phone" placeholder="Chọn ảnh">
+                        </div>
+                        <div class="form-group">
+                          <label>Email</label>
+                          <input class="form-control" id="email" name="email"placeholder="Nhập email">
+                        </div>
+                      </div>
+                      <!-- /.box-body -->
+
+                      <div class="box-footer">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                      </div>
+                    </form>
+                  </div>
 @endsection
