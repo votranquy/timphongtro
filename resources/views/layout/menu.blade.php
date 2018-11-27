014f5e339aa994a24b135a4e746c60f9934219b3
votranquy <votranquy96@gmail.com>
2018-11-27 23:39:43 +0700

    fix

--- resources/views/layout/menu.blade.php
+++ resources/views/layout/menu.blade.php
@@ -4,23 +4,19 @@
 			<div class="module_title">
 				<span>Tin đăng chính chủ</span>
 			</div>
-			<ul class="list_pro">
+			<ul class="list">
 				@foreach($baichinhchu as $bcc)
 				<li>
 					@if( count($bcc->anh) != 0)
 					<?php $tenanh= $bcc->anh->first();?>
-					<a class="img"
-					href=""
-					title="">
-						<img
-						alt=""
-						src="upload/tintuc/{{$tenanh->path}}">
-					</a><br>
-					@endif
+					<a class="img" href="" title="">
+						<img alt="" src="upload/tintuc/{{$tenanh->path}}">
+					</a>
 					<a class="title" href="">
 						{{ $bcc->title }}
 					</a>
 					<p class="price">{{ $bcc->price }}</p>
+					@endif
 				</li>
 				@endforeach
 			</ul>
@@ -33,19 +29,14 @@
 			<ul class="list">
 				@foreach($baimoi as $bm)
 				<li>
-					<a class="img"
-					href=""
-					title="">
-						<img
-						alt=""
-						<?php $tenanh= $bm->anh->first();?>
-						src="upload/tintuc/{{$tenanh->path}}">
-					</a> 
-					<a class="title"
-					href="">
+					<?php $tenanh= $bm->anh->first();?>
+					<a class="img" href="" title="">
+						<img alt="" src="upload/tintuc/{{$tenanh->path}}">
+					</a>
+					<a class="title" href="">
 						{{$bm->title}}
 					</a>
-					<p class="price">{{$bm->chitietphong->price}}</p>
+					<!-- <p class="price">{{$bm->chitietphong->price}}</p> -->
 				</li>
 				@endforeach
 			</ul>
