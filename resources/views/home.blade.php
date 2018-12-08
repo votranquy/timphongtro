ba82229b2c8f556576bf36d1c6ef6441c2af75fc
votranquy <votranquy96@gmail.com>
2018-12-08 16:37:53 +0700

    fixcodewithnotesamefile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,8 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    You are logged in! How do you feel??
+                    
                 </div>
             </div>
         </div>
