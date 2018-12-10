882ce82188c6f0a03195d184c50001426bf00efe
votranquy <votranquy96@gmail.com>
2018-12-10 21:34:14 +0700

    FIX

--- database/seeds/users.php
+++ database/seeds/users.php
@@ -14,8 +14,7 @@ class users extends Seeder
     	DB::table('users')->insert([
         	['id'=>'1','username' => 'admin','password' =>bcrypt('12345678') ,'name' => 'Le Admin1q','address'=>'44 Hàm Nghi, Đà Nẵng','phone'=>'1234567899','image'=>'','email'=>'admin@gmail.com'],
         	['id'=>'2','username' => 'mod','password' => bcrypt('12345678'),'name' => 'Le B','address'=>'45 Hàm Nghi, Đà Nẵng','phone'=>'122344','image'=>'','email'=>'mod@gmail.com'],
-        	 ['id'=>'3','username' => 'user','password' => bcrypt('12345678'),'name' => 'Le C','address'=>'44 Hàm Nghi','phone'=>'122344','image'=>'','email'=>'user@gmail.com'],
-            ['id'=>'4','username' => 'Võ Trần Quý','password' => bcrypt('12345678'),'name' => 'Võ Trần Quý','address'=>'44 Hàm Nghi','phone'=>'0369380628','image'=>'','email'=>'votranquy96@gmail.com']
+        	 ['id'=>'3','username' => 'user','password' => bcrypt('12345678'),'name' => 'Le C','address'=>'44 Hàm Nghi','phone'=>'122344','image'=>'','email'=>'user@gmail.com']
     	]);
 
     }
