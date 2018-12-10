c8345cecbd21fe89a5d91a11a5737b46e6023a6e
votranquy <votranquy96@gmail.com>
2018-12-10 22:08:14 +0700

    D

--- database/seeds/rooms_detail.php
+++ database/seeds/rooms_detail.php
@@ -11,6 +11,8 @@ class rooms_detail extends Seeder
      */
     public function run()
     {
-
+    	DB::table('rooms_detail')->insert([
+        	['id'=>'1','post_id' => '1','aceage' => '1','minAceage'=>NULL,'maxAceage'=>NULL,'description'=>NULL,'longitute'=>'300000','latitude'=>'1500000','created_at'=>NOW(),'updated_at'=>NOW()]
+    	]);
     }
 }
