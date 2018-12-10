9ceada005f3bf85443854eaef07026c43f782fbc
votranquy <votranquy96@gmail.com>
2018-12-10 21:02:30 +0700

    add

--- database/seeds/rooms_detail.php
+++ database/seeds/rooms_detail.php
@@ -11,9 +11,6 @@ class rooms_detail extends Seeder
      */
     public function run()
     {
-    	DB::table('rooms_detail')->insert([
-        	['id'=>'1','post_id' => '1','aceage'=>NULL,'minAceage' => '15','maxAceage' => '20','description'=>'Cần tìm trọ gấp, muốn phòng gần đại học BK, an ninh tốt, chủ trọ vui tính','longitute'=>'','latitude'=>''],
-        	['id'=>'2','post_id' => '2','aceage'=>'30','minAceage' => NULL,'maxAceage' => NULL,'description'=>'Câng cho thuê phòng đường Ngô Thì Nhậm có thể ở từ 2-4 người, vệ sinh tốt, anh ninh đảm bảo','longitute'=>'16.069847','latitude'=>'108.151994']
-    	]);
+
     }
 }
